428aafaf18de35a05097cbf122fb63d0561ad964
Viet Anh <skynet0590@gmail.com>
2025-03-03 15:58:24 +0700

    complete exchcx exchange

--- instantswap/exchange/exchcx/exchcx.go
+++ instantswap/exchange/exchcx/exchcx.go
@@ -6,6 +6,7 @@ import (
 	"github.com/crypto-power/instantswap/instantswap"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -81,6 +82,42 @@ func (e *exchCx) QueryLimits(fromCurr, toCurr string) (res instantswap.QueryLimi
 }
 
 func (e *exchCx) CreateOrder(vars instantswap.CreateOrder) (res instantswap.CreateResultInfo, err error) {
+	var params = url.Values{}
+	params.Set("from_currency", vars.FromCurrency)
+	params.Set("to_currency", vars.ToCurrency)
+	params.Set("to_address", vars.Destination)
+	params.Set("refund_address", vars.RefundAddress)
+	params.Set("rate_mode", "flat")
+	params.Set("fee_option", "s")
+	req, err := http.NewRequest(http.MethodGet, e.path("create?"+params.Encode()), nil)
+	if err != nil {
+		return res, err
+	}
+	var createResponse struct {
+		OrderId string `json:"orderid"`
+	}
+	err = e.Do(req, &createResponse)
+	if err != nil {
+		return res, err
+	}
+	order, err := e.getOrder(createResponse.OrderId)
+	if err != nil {
+		return res, err
+	}
+	res = instantswap.CreateResultInfo{
+		ChargedFee:     0,
+		Destination:    order.ToAddress,
+		ExchangeRate:   order.Rate,
+		FromCurrency:   order.FromCurrency,
+		InvoicedAmount: 0,
+		OrderedAmount:  0,
+		ToCurrency:     order.ToCurrency,
+		UUID:           "",
+		DepositAddress: order.FromAddr,
+		Expires:        0,
+		ExtraID:        "",
+		PayoutExtraID:  "",
+	}
 	return
 }
 
@@ -92,10 +129,68 @@ func (e *exchCx) CancelOrder(orderID string) (res string, err error) {
 	return
 }
 
+func (e *exchCx) getOrder(orderId string) (*Order, error) {
+	req, err := http.NewRequest(http.MethodGet, e.path("order?orderid="+orderId), nil)
+	if err != nil {
+		return nil, err
+	}
+	var order Order
+	err = e.Do(req, &order)
+	if err != nil {
+		return nil, err
+	}
+	return &order, nil
+}
+
 func (e *exchCx) OrderInfo(orderID string, extraIds ...string) (res instantswap.OrderInfoResult, err error) {
+	order, err := e.getOrder(orderID)
+	if err != nil {
+		return
+	}
+	res = instantswap.OrderInfoResult{
+		Expires:        0,
+		LastUpdate:     "",
+		ReceiveAmount:  *order.ToAmount,
+		TxID:           *order.TransactionIdSent,
+		Status:         order.State,
+		InternalStatus: statusMap[order.State],
+		Confirmations:  "",
+	}
 	return
 }
 
 func (e *exchCx) GetExchangeRateInfo(vars instantswap.ExchangeRateRequest) (res instantswap.ExchangeRateInfo, err error) {
+	req, err := http.NewRequest(http.MethodGet, e.path("rates"), nil)
+	if err != nil {
+		return
+	}
+	var rateMap map[string]Rate
+	err = e.Do(req, &rateMap)
+	if err != nil {
+		return
+	}
+	var pair = strings.ToUpper(vars.From) + "_" + strings.ToUpper(vars.To)
+	var rate, ok = rateMap[pair]
+	if !ok {
+		err = fmt.Errorf("exchange rate info not found")
+		return
+	}
+	res.ExchangeRate = rate.Rate
 	return
+}
+
+var statusMap = map[string]instantswap.Status{
+	"CREATED":           instantswap.OrderStatusNew,
+	"CANCELLED":         instantswap.OrderStatusCanceled,
+	"AWAITING_INPUT":    instantswap.OrderStatusWaitingForDeposit,
+	"CONFIRMING_INPUT":  instantswap.OrderStatusWaitingForDeposit,
+	"EXCHANGING":        instantswap.OrderStatusExchanging,
+	"FUNDED":            instantswap.OrderStatusDepositConfirmed,
+	"BRIDGING":          instantswap.OrderStatusSending,
+	"CONFIRMING_SEND":   instantswap.OrderStatusSending,
+	"COMPLETE":          instantswap.OrderStatusCompleted,
+	"REFUND_REQUEST":    instantswap.OrderStatusRefunded,
+	"REFUND_PENDING":    instantswap.OrderStatusRefunded,
+	"CONFIRMING_REFUND": instantswap.OrderStatusRefunded,
+	"REFUNDED":          instantswap.OrderStatusRefunded,
 }
--- instantswap/exchange/exchcx/structs.go
+++ instantswap/exchange/exchcx/structs.go
@@ -14,8 +14,28 @@ type Rate struct {
 		M string `json:"m"`
 		S string `json:"s"`
 	} `json:"network_fee"`
-	Rate     string `json:"rate"`
-	RateMode string `json:"rate_mode"`
-	Reserve  string `json:"reserve"`
-	SvcFee   string `json:"svc_fee"`
+	Rate     float64 `json:"rate,string"`
+	RateMode string  `json:"rate_mode"`
+	Reserve  any     `json:"reserve"`
+	SvcFee   string  `json:"svc_fee"`
+}
+
+type Order struct {
+	Created               int      `json:"created"`
+	FromAddr              string   `json:"from_addr"`
+	FromAmountReceived    *float64 `json:"from_amount_received"`
+	FromCurrency          string   `json:"from_currency"`
+	MaxInput              string   `json:"max_input"`
+	MinInput              string   `json:"min_input"`
+	NetworkFee            int      `json:"network_fee"`
+	OrderId               string   `json:"orderid"`
+	Rate                  float64  `json:"rate,string"`
+	RateMode              string   `json:"rate_mode"`
+	State                 string   `json:"state"`
+	SvcFee                string   `json:"svc_fee"`
+	ToAddress             string   `json:"to_address"`
+	ToAmount              *float64 `json:"to_amount"`
+	ToCurrency            string   `json:"to_currency"`
+	TransactionIdReceived *string  `json:"transaction_id_received"`
+	TransactionIdSent     *string  `json:"transaction_id_sent"`
 }
